7a9c8685f3e91ebced0289ba651df722ac80817d
Rodrigo Campuzano <camcluod@gmail.com>
2025-03-07 19:19:32 -0600

    .

--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
 	"log"
 	"net/http"
 
-	"API_ONE/src/core/db"
-	"API_ONE/src/core/middleware"
 	"API_ONE/src/esp32/infraestructure/routes"
 )
 
 func main() {
-	db.Init()
 
 	router := routes.NewVentaRouter()
 
-	handler := middleware.CORS(router)
 
 	log.Println("API escuchando en :8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
